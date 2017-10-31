72669af983706c8cb8dc115d487d504fd8cbe9c3
ARoyCode <aditi.roy84@gmail.com>
2017-10-31 14:49:46 -0400

    Update train_with_dataaug.py

--- model/train_with_dataaug.py
+++ model/train_with_dataaug.py
@@ -15,10 +15,10 @@ batch_size = 64
 num_classes = 10
 epochs = 10
 
-# input image dimensions
+#Input image dimensions
 img_rows, img_cols = 28, 28
 
-# the data, shuffled and split between train and test sets
+#Load the data. Then shuffle and split it between train and test sets.
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
 if K.image_data_format() == 'channels_first':
@@ -39,14 +39,13 @@ print(x_train.shape[0], 'train samples')
 print(x_test.shape[0], 'test samples')
 
 #print('Before converting to binary class y_train[0]:', y_train[0])
-# convert class vectors to binary class matrices
+#Convert class vectors to binary class matrices
 y_train = keras.utils.to_categorical(y_train, num_classes)
 y_test = keras.utils.to_categorical(y_test, num_classes)
-
 #print('After converting to binary class y_train[0]:',y_train[0])
 
 
-
+# Create the CNN model with batch normalization, max pooling and drop out
 model = Sequential()
 model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=input_shape))
 model.add(Activation('relu'))
@@ -63,15 +62,13 @@ model.add(Conv2D(64, (3, 3)))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2,2)))
 
-
-#model.add(Dropout(0.25))
 model.add(Flatten()) # Fully connected layer
 model.add(Dense(128, activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(num_classes, activation='softmax'))
 
 #print(model.summary())
-
+#Complie the model
 model.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adadelta(), metrics=['accuracy'])
 
 # Perform Data augmentation
@@ -85,13 +82,15 @@ test_gen = ImageDataGenerator()
 train_generator = gen.flow(x_train, y_train, batch_size=batch_size)
 test_generator = test_gen.flow(x_test, y_test, batch_size=batch_size)
 
+#Perform model training with augmented data
 model.fit_generator(train_generator, steps_per_epoch=60000//64, epochs=epochs, validation_data=test_generator, validation_steps=10000//64)
 
+#Evaluate the model
 score = model.evaluate(x_test, y_test, verbose=0)
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
 
-
+#Save model parameters
 with open('model.json', 'w') as outfile:
     json.dump(model.to_json(), outfile)
 model.save_weights('weights_withDA.h5')
